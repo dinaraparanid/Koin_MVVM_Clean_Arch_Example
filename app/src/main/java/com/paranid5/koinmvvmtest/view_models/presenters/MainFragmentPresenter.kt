64ee48f19f324e080ee5dd4eb069d50528db7c13
dinaraparanid <dinaraparanid@gmail.com>
2022-11-28 01:27:28 +0300

    readme + enable confirm button

--- app/src/main/java/com/paranid5/koinmvvmtest/view_models/presenters/MainFragmentPresenter.kt
+++ app/src/main/java/com/paranid5/koinmvvmtest/view_models/presenters/MainFragmentPresenter.kt
@@ -3,6 +3,9 @@ package com.paranid5.koinmvvmtest.view_models.presenters
 import androidx.databinding.Bindable
 import com.paranid5.koinmvvmtest.BR
 
+private inline val CharSequence.isValidName
+    get() = isNotEmpty() && all { it.isLetterOrDigit() || it == ' ' } && "  " !in this
+
 class MainFragmentPresenter(nameContainerText: String) : BasePresenter() {
     @get:Bindable
     var nameContainerText = nameContainerText
@@ -12,4 +15,18 @@ class MainFragmentPresenter(nameContainerText: String) : BasePresenter() {
             field = value
             notifyPropertyChanged(BR.nameContainerText)
         }
+
+    @get:Bindable
+    var isConfirmButtonActive = false
+        @JvmName("isConfirmButtonActive") get
+        @JvmName("setConfirmButtonActive")
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.confirmButtonActive)
+        }
+
+    @JvmName("onInputNameTextChanged")
+    fun onInputNameTextChanged(s: CharSequence?) {
+        isConfirmButtonActive = s?.isValidName == true
+    }
 }
